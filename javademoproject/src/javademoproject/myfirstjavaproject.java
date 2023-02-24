96aceca19656c2fc9f4ab389e71375ec292a773d
Amulya V R <avr@BLR.ALLEGISINDIA.COM>
2023-02-24 08:58:37 +0530

    created my first project

---
+++ javademoproject/src/javademoproject/myfirstjavaproject.java
@@ -0,0 +1,7 @@
+package javademoproject;
+
+public class myfirstjavaproject {
+public static void main(String[] args) {
+	System.out.println("hello world!");
+}
+}
---
+++ javademoproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author avr
+ *
+ */
+module javademoproject {
+}
